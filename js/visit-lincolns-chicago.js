d1b60c603e48c45319c6a096739a7c208d68f288
Enrique Gonalez <egonzalez78@gmail.com>
2014-04-22 09:07:36 -0500

    added document ready to prevent global namespace pollution

--- js/visit-lincolns-chicago.js
+++ js/visit-lincolns-chicago.js
@@ -1,8 +1,13 @@
-$("dl.menu").on("click", "dd > a", function() {
 
-	var $menuItem = $("dd > a");
+$(document).ready(function() {
 
-	$menuItem.removeClass("menu--tour--active")
-	$(this).addClass("menu--tour--active"); 
+	$("dl.menu").on("click", "dd > a", function() {
+
+		var $menuItem = $("dd > a");
+
+		$menuItem.removeClass("menu--tour--active")
+		$(this).addClass("menu--tour--active"); 
+
+	});
 
 });
